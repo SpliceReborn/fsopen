fd5fbe14135fcb4826a4c7104cb493aa0b6d945e
Darren <splicereborn@gmail.com>
2022-08-08 23:32:54 +0800

    2.11: The Phonebook Step6 (remove redundant{}

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({filterWord, handleChange}) => (
   <p>
@@ -27,24 +28,24 @@ const Persons = ({filterWord, persons}) => {
     person.name.toLowerCase().includes(filterWord)
   )
 
-  {return filterWord === '' ? 
+  return filterWord === '' ? 
     persons.map(person => <Person key={person.id} name={person.name} number={person.number} />) :  
     filtered.map(person => <Person key={person.id} name={person.name} number={person.number} />)
-  }
 }
 
 const Person = ({name, number}) => <p>{name} {number}</p>
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567', id: 1},
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterWord, setFilterWord] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   function handleChange(event) {
     if (event.target.name === "name") 
