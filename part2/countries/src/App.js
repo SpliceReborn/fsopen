43345f1a31fc5e8603921c6e1595cde359e302a9
Darren <splicereborn@gmail.com>
2022-08-09 01:02:38 +0800

    2.13*: Data for countries, step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,28 @@
 import axios from 'axios'
 import { useState,  useEffect } from 'react';
 
-function App() {
+const Country = ({country}) => {
+  const name = country.name.common
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>
+        capital {country.capital} <br/>
+        area {country.area}
+      </p>
+      <p><b>languages:</b></p>
+      <ul>
+        {
+          Object.values(country.languages)
+            .map(language => <li key={language}>{language}</li>)
+        }
+      </ul>
+      <img width="175px" src={country.flags.png} alt={`${name}'s flag`} />
+    </div>
+  )
+}
+
+const App = () => {
 
   const [countries, setCountries] = useState([])
   useEffect(() => {
@@ -13,6 +34,12 @@ function App() {
   const [search, setSearch] = useState('')
   function handleChange(event) {
     setSearch(event.target.value)
+    setCountryView({})
+  }
+
+  const [countryView, setCountryView] = useState({})
+  function showCountry(country) {
+    setCountryView(country)
   }
 
   const matched = countries.filter(country => country.name.common.toLowerCase().includes(search))
@@ -20,27 +47,15 @@ function App() {
   // No need to useState because search field rerenders view anyway..
   let display = <></>
   if (matched.length === 1) {
-    const country = matched[0]
-    const name = country.name.common //country's name is an object
-    console.log(country)
-    display = 
-      <div>
-        <h2>{name}</h2>
-        <p>
-          capital {country.capital} <br/>
-          area {country.area}
-        </p>
-        <p><b>languages:</b></p>
-        <ul>
-          {
-            Object.values(country.languages)
-              .map(language => <li key={language}>{language}</li>)
-          }
-        </ul>
-        <img width="175px" src={country.flags.png} alt={`${name}'s flag`} />
-      </div>
+    display = <Country country={matched[0]}/>
   } else if (matched.length < 10) {
-    display = matched.map(country => <p key={country.name.common}>{country.name.common}</p>)
+    display = matched.map(country => {
+      return (
+        <p key={country.name.common}>{country.name.common}
+          &#160;<button onClick={() => showCountry(country)}>show</button>
+        </p>
+      )
+    })
   } else {
     display = <p>Too many matches, specify another filter</p>
   }
@@ -49,6 +64,7 @@ function App() {
     <div>
       <p>find countries <input value={search} onChange={handleChange}/></p>
       { !!search && display }
+      { !!Object.keys(countryView).length ? <Country country={countryView} /> : <></> }
     </div>
   );
 }
